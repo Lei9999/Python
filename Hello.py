dc71559f19758e451ecdfeeb11dfa4465f19dc33
Lei9999 <shilei66491@126.com>
2021-01-06 16:45:45 +0800

    commit

--- Hello.py
+++ Hello.py
@@ -1,4 +1,9 @@
-# -*- coding: uft-8 -*-
+# -*- coding: utf-8 -*-
 
-a = "Hello, World!"
+#a = "Hello, World!"
+#print(a)
+
+import random
+from random import choices
+a = random.choices([1, 2, 3, 4, 5], k=3)
 print(a)
--- Work/work3.py
+++ Work/work3.py
@@ -4,5 +4,23 @@
 # 并要求为每道题输入正确答案，正确率百分比大于等于60%输出结果pass，
 # 否则输出结果fail。将结果写入文件，文件名由答题者自定义。
 
-questions_list = [1, 2, 3, 4, 5]
-print(questions_list)
+import random
+from random import sample
+
+questions = {'\n题目1\n\nA\nB\nC\nD\n',
+'\n题目2\n\nA\nB\nC\nD\n',
+'\n题目3\n\nA\nB\nC\nD\n',
+'\n题目4\n\nA\nB\nC\nD\n',
+'\n题目5\n\nA\nB\nC\nD\n'}
+correctanswer = {'A', 'C', 'D', 'A', 'B'}
+#len(questions)
+#print(len(questions))
+questions_list = list(questions)
+choice_id = sample([0, 1, 2, 3, 4], k=3)
+#print(choice_id)
+for i in range(3):
+    #print(i)
+    choice_index = choice_id[i]
+    #print('ID = ', a)
+    current_questions = questions_list[choice_index]
+    answer = input(current_questions + "\n请输入答案：")
