6aef900e755a17d097f7481274972872af0bd2c1
Lei9999 <shilei66491@126.com>
2021-02-09 10:29:55 +0800

    20210209

--- Learn/学习笔记/文件的读写.py
+++ Learn/学习笔记/文件的读写.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 
-#import 
+# open 函数的使用
+
+files1 = open('C:\\Users\\root\\Desktop\\Workspace\\devices.txt', 'r')
+files2 = open('C:\\Users\\root\\Desktop\\Workspace\\IP_Lists.txt', 'r')
+#print(files)
+#devices_list = files1.readlines()
+#ip_list = files2.readlines()
+#print(devices_list)
+#print(ip_list)
+
+for devices_list in files1.readlines():
+    print(devices_list)
+
+for ip_list in files2.readlines():
+    print(ip_list)
