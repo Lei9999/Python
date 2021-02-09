5445ae5645b5ffdfcbbd901078ada8b9d331bd27
Lei9999 <shilei66491@126.com>
2021-02-09 11:24:02 +0800

    20210209

--- Learn/学习笔记/文件的读写.py
+++ Learn/学习笔记/文件的读写.py
@@ -2,16 +2,21 @@
 
 # open 函数的使用
 
-files1 = open('C:\\Users\\root\\Desktop\\Workspace\\devices.txt', 'r')
-files2 = open('C:\\Users\\root\\Desktop\\Workspace\\IP_Lists.txt', 'r')
+device_files = open('C:\\Users\\root\\Desktop\\Workspace\\devices.txt', 'r')
+ip_files = open('C:\\Users\\root\\Desktop\\Workspace\\IP_Lists.txt', 'r')
 #print(files)
-#devices_list = files1.readlines()
-#ip_list = files2.readlines()
+#devices_list = device_files.readlines()
+#ip_list = ip_files.readlines()
 #print(devices_list)
 #print(ip_list)
 
-for devices_list in files1.readlines():
-    print(devices_list)
+#for devices_list in device_files.readlines():
+    #print(devices_list)
 
-for ip_list in files2.readlines():
-    print(ip_list)
+
+#for ip_list in files2.readlines():
+    #print(ip_list)
+
+for ip in ip_files.readlines():
+    if ip.startswith('172.16'):
+        print(ip.strip())  # 删除换行符
