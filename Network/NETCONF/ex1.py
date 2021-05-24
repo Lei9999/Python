1baf812aaf4005e26de33cb3365b9ebb07fe0a94
Lei9999 <shilei66491@126.com>
2021-05-24 15:39:31 +0800

    20210524-15:32

---
+++ Network/NETCONF/ex1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import ncclient
+from ncclient import manager
+
+Host = ''
+User = 'admin'
+Password = ''
+
+with manager.connect(host=Host, port=830, username=User, hostkey_verify=False, device_params={'name':'csr'}) as m:
+    c = m.get_config(source='running').data_xml
+    with open("%s.xml" % Host, 'w') as f:
+        f.write(c)
